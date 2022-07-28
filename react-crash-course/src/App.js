9f1ca8c0d5482c6b0416edd53198e34a9d6f9199
dmitrysidorhik <dmitrysidorhik@gmail.com>
2022-07-28 16:49:26 +0300

    work for add  scss

--- react-crash-course/src/App.js
+++ react-crash-course/src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
 
-function App() {
+const App = () => {
   return (
-<></>
+    <>
+      <Header />
+    </>
   );
-}
+};
 
 export default App;
--- react-crash-course/src/components/Header/Header.jsx
+++ react-crash-course/src/components/Header/Header.jsx
@@ -1,11 +1,27 @@
-import React from "react";
-import LogoImage from '../../assets/img/logo.svg' 
+import React from 'react'
+import LogoImage from '../../assets/img/logo.svg'
+import { menu } from './menu'
+
+import styles from './Header.module.scss'
+
 const Header = () => {
     return (
         <>
             <div className="logo">
                 <img src={LogoImage} alt='' />
-
+            </div>
+            <div className="menu-wrapper">
+                <ul>
+                    {menu.map((item, idx) => (
+                        <li key={`menu item ${idx}`}>
+                            <a href={item.link}>{item.title}</a>
+                        </li>
+                    ))}
+                </ul>
+                <div className="auth-buttons">
+                    <button className="login">Login</button>
+                    <button className="sign-up">Sign up</button>
+                </div>
             </div>
         </>
     )
